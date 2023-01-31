b1348dfb34d1a62e62c8b64ba134f52bdd9f3be0
unknown <vasuv0503@gmail.com>
2023-01-31 08:54:23 +0530

    initial commit

---
+++ src/Tutorfrom19/Class20.java
@@ -0,0 +1,29 @@
+package Tutorfrom19;
+
+//fulfilling second important thing
+
+import MissionTest1.Beginner3;
+
+public class Class20 {
+
+	public static void main(String[] args) {
+		// What are packages in JAva
+		/*
+		 * packages are nothing but grouping of related classes or instances
+		 * provides access specifier protection -> restrict access to certain classes
+		 * two main important things 
+		 * 1. To access methods of different class of same package -> we have to create an object for the class of the 
+		 * method we are using in the new class
+		 * 2. To use use method of different class of different package -> you have to import that class
+		 */
+		//using method of different class in same package -> class19 in class 20 //first important thing
+		class19 meth = new class19();
+		meth.doLogin();
+		
+		//second important thing // using method of different class in different package
+		Beginner3 metho = new Beginner3();
+		metho.doLogout();
+
+	}
+
+}
